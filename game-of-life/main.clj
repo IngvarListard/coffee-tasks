bff470fe0a49e4303271906b3399719346e35757
IngvarListard <vlzvoice@ya.ru>
2022-02-20 16:37:14 +0300

    game of life

--- game-of-life/main.clj
+++ game-of-life/main.clj
@@ -1,6 +1,7 @@
 (ns game-of-life
   (:require
-   [clojure.pprint :refer [pprint]]))
+   [clojure.pprint :refer [pprint]]
+   [clojure.zip :as z]))
 ;; реализация игры в жизнь из книги Clojure Programming
 
 
@@ -245,7 +246,75 @@
 
 ;; (main)
 
+(def labyrinth (let [g (grid 10 10)] (reduce disj g (maze g))))
+
+(def theseus (rand-nth (distinct (apply concat labyrinth))))
+(def minotaur (rand-nth (distinct (apply concat labyrinth))))
+
+
+(defn ariadne-zip
+  [labyrinth loc]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq labyrinth))
+        children (fn [[from to]]
+                   (seq (for [loc (paths to)
+                              :when (not= loc from)]
+                          [to loc])))]
+    (z/zipper (constantly true)
+              children
+              nil
+              [nil loc])))
+
+
+(defn draw
+  [w h maze path]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+     (doto (proxy [javax.swing.JPanel] []
+             (paintComponent [^java.awt.Graphics g]
+               (let [g (doto ^java.awt.Graphics2D (.create g)
+                         (.scale 10 10)
+                         (.translate 1.5 1.5)
+                         (.setStroke (java.awt.BasicStroke. 0.4)))]
+                 (.drawRect g -1 -1 w h)
+                 (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                   (let [[xc yc] (if (= xa xb)
+                                   [(dec xa) ya]
+                                   [xa (dec ya)])]
+                     (.drawLine g xa ya xc yc)))
+                 (.translate g -0.5 -0.5)
+                 (.setColor g java.awt.Color/RED)
+                 (doseq [[[xa ya] [xb yb]] path]
+                   (.drawLine g xa ya xb yb)))))
+       (.setPreferredSize (java.awt.Dimension.
+                           (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
+
+
 (comment
+(let [w 40, h 40
+      grid (grid w h)
+      walls (maze grid)
+      labyrinth (reduce disj grid walls)
+      places (distinct (apply concat labyrinth))
+      theseus (rand-nth places)
+      minotaur (rand-nth places)
+      path (->> theseus
+                (ariadne-zip labyrinth)
+                (iterate z/next)
+                (filter #(= minotaur (first (z/node %))))
+                first z/path rest)]
+  (draw w h walls path))
+
+
+  (->> theseus
+       (ariadne-zip labyrinth)
+       (iterate z/next)
+       (filter #(= minotaur (second (z/node %))))
+       first z/path
+       (map second))
 
   (draw 40 40 (maze (grid 40 40)))
   (hex-draw 40 40 (maze (hex-grid 40 40)))
